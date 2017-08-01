fa0cd0fd9b39da83fadbec02c220c93039c20f5e
shy <shyamal.punekar@gmail.com>
2017-07-31 21:31:37 -0700

    Add all possible scenarios to unit test

--- src/main/java/models/PingPong.java
+++ src/main/java/models/PingPong.java
@@ -8,7 +8,18 @@ public class PingPong {
 
     public List<Object> runPingPong(int countUpTo){
         List<Object> result = new ArrayList<Object>();
-        return null;
+        for (int i =1 ; i<=countUpTo ; i++) {
+            if (i % 3 == 0 && i % 5 == 0){
+                result.add("pingpong");
+            } else if (i % 3 ==0) {
+                result.add("ping");
+            } else if (i % 5 == 0){
+                result.add("pong");
+            } else {
+                result.add(i);
+            }
+        }
+        return result;
     }
 
 }
